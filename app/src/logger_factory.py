f73ba91b7d9000f0cebdc7abdb593d652d22f5d6
Johannes Sarpola <johannessarpola@users.noreply.github.com>
2018-04-06 19:38:53 +0300

    Adds failsafes for logger_factory

--- app/src/logger_factory.py
+++ app/src/logger_factory.py
@@ -3,6 +3,7 @@ from logging.handlers import RotatingFileHandler
 from os import path, makedirs
 
 class LoggerFactory(object):
+    root_logger = None
     loggers = {}
     file_handler = None
     console_handler = None
@@ -12,16 +13,24 @@ class LoggerFactory(object):
 
         dest = path.join(dir, fname)
 
-        self.file_handler = RotatingFileHandler(dest, maxBytes=log_file_size, backupCount=backups_count)
-        self.file_handler.setFormatter(logFormatter)
-
         self.console_handler = logging.StreamHandler()
         self.console_handler.setFormatter(logFormatter)
+        self.root_logger = logging.getLogger(__name__)
+        self.root_logger.setLevel(logging.DEBUG)
+        self.root_logger.addHandler(self.console_handler)
 
-        if not path.exists(dest):
-            # TODO Test that this actually works
-            self.instance(__name__).debug(f"Initializing logs folder to: {dest}")
-            makedirs(dest)
+        try:
+            self.file_handler = RotatingFileHandler(dest, maxBytes=log_file_size, backupCount=backups_count)
+            self.file_handler.setFormatter(logFormatter)
+            if not path.exists(dest):
+                self.root_logger.debug(f"Initializing logs folder to: {dest}")
+                makedirs(dest)
+            self.root_logger.addHandler(self.file_handler)
+        except:
+            self.root_logger.warning(f"Could not initialize logs folder: {dest}")
+            self.file_handler = None
+
+
 
 
     def instance(self, name, level = logging.DEBUG):
@@ -31,7 +40,8 @@ class LoggerFactory(object):
             logger = logging.getLogger(name)
             logger.setLevel(logging.DEBUG)
             logger.addHandler(self.console_handler)
-            logger.addHandler(self.file_handler)
+            if self.file_handler is not None:
+                logger.addHandler(self.file_handler)
             self.loggers[name] = logger
             return logger
 
